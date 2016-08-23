7fe9cd25697ff70b0d87ddf4bad061f0889b48d0
alex-felix <alexander.benoit@outlook.com>
2016-08-23 14:28:17 +0100

    Update location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
 <<<<<<< HEAD
-  validates :title, :description, :address, :city, presence: true
-  validates :postcode, presence: true, format: {
+  validates :title, :description, :address, :city, :postcode, presence: true
+  validates :postcode, format: {
     with: /\A([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)\z/,
     message: "invalid poscode"
   }
