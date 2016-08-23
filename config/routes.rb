d01909ce09f99eeba778cbac9e42b48c9fa8e51b
Alex Benoit <alexander.benoit@outlook.com>
2016-08-23 23:29:44 +0100

    updated some stuff (footer, navbar, put in root at events

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :events do
     resources :attendances, only: [:create]
   end
-  root to: 'pages#home'
+  resources :locations, only: [:index, :create, :new]
+  root to: 'events#index'
   get 'contact', to: 'pages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
