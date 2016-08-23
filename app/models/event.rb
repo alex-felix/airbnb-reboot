a41d75ff04f047fe590b6a47096b4f9078e7a661
Alex Benoit <alexander.benoit@outlook.com>
2016-08-23 16:58:25 +0100

    alex2

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: true }
-  validates :description, presence: true, length: { minimum: 25 }
   has_many :attendances
   has_many :users, through: :attendances
   belongs_to :location
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: true }
+  validates :description, presence: true, length: { minimum: 25 }
   validates :user, :location, :starts_at, :ends_at, presence: true
   # validates :interests, inclusion: { in: %w(outdoors citytrip sport nightlife home) }
 end
