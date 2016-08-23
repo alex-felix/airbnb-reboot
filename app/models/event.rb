9cd61de0451fc3a50652f568d4bac974c187a68b
Nicolas Dotto <nicolas.dotto@hotmail.com>
2016-08-23 14:12:00 +0100

    j'ai fini les validationes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: true }
+  validates :description, presence: true, length: { minimum: 25 }
   belongs_to :location
   belongs_to :user
+  validates :user, :location, :start_time, :end_time, presence: true
+  CATEGORIES = %w(outdoors citytrip sport nightlife home)
+  validates :category, inclusion: { in: CATEGORIES }
 end
