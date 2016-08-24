b17e6260866f036624568526e9afe4f097270216
Michael Dusong <michael@regiomag.net>
2016-08-24 14:41:09 +0100

    Added date_time and _date_day column

---
+++ db/migrate/20160824132949_add_column_to_events.rb
@@ -0,0 +1,6 @@
+class AddColumnToEvents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :events, :starts_at_day, :date
+    add_column :events, :ends_at_day, :date
+  end
+end
---
+++ db/migrate/20160824133018_add_time_to_events.rb
@@ -0,0 +1,6 @@
+class AddTimeToEvents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :events, :starts_at_time, :time
+    add_column :events, :ends_at_time, :time
+  end
+end
