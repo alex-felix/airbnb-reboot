3d75b18aff2b0105afb02f476fb85c98f13b5b09
Nicolas Dotto <nicolas.dotto@hotmail.com>
2016-08-25 18:39:41 +0100

    maps nearrly working

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 Geocoder.configure(
+  :lookup    => :google,
+  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  :use_https => true,
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :google,            # name of geocoding service (symbol)
@@ -20,3 +23,4 @@ Geocoder.configure(
   units: :km, # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
+
