eb3a9dc5567b0e5dfdb08b28725603bebcfa68fa
Michael Dusong <michael@regiomag.net>
2016-08-25 13:18:05 +0100

    basic profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
 class ProfilesController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+    @events = Event.where(user_id: current_user.id)
+  end
+
+  private
+  # To filter hacking forms
+  def user_params
+    params.require(:user).permit(:name, :address)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   end
   root to: 'events#index'
   get 'contact', to: 'pages#contact'
+  resources :profiles, only: [:show , :update]
 end
