94666b1a747e5ea4bea6efed10b4f8695f3b8606
Alex Benoit <alexander.benoit@outlook.com>
2016-08-26 10:32:31 +0100

    fixed seed spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # location3 = Location.create!(name: "King's Palace", description: 'Best italian in victoria', address: 'shoreditch road', postcode: 'E1 6BT', city: 'London', country: 'UK')
 # location4 = Location.create!(name: 'Chez Gervais', description: 'Best french in south kensington', address: 'shoreditch road', postcode: 'W8 6EF', city: 'London', country: 'UK')
 
-sam = User.create!  (first_name: 'Sam', last_name: 'Appleseed', address: 'Shoreditch', city: 'London', gender: 'Male', email: 'sam.appleseed@gmail.com', phone_number: '07449949576', postcode: 'W8 6EF', date_of_birth: Date.new(1994, 8, 29), password: 'toto')
+sam = User.create!(first_name: 'Sam', last_name: 'Appleseed', address: 'Shoreditch', city: 'London', gender: 'Male', email: 'sam.appleseed@gmail.com', phone_number: '07449949576', postcode: 'W8 6EF', date_of_birth: Date.new(1994, 8, 29), password: 'toto')
 mike = User.create!(first_name: 'Mike', last_name: 'Julien', address: 'Pimlico', city: 'London', gender: 'Male', email: 'mike.julien@gmail.com', phone_number: '07449949576', postcode: 'W8 6EF', date_of_birth: Date.new(1969, 8, 29), password: 'titi')
 
 Event.create!(title: 'Movie Night: Finding Dory', description: 'I am organising a viewing of Finding Dory on my brand new home cinema. Join me if you am up to watching it!', interests: ['movies', 'movie night', 'chill'], starts_at_day: Date.new(2016, 8, 26), ends_at_day: Date.new(2016, 8, 26), user: sam, address: '68 Leonard St', postcode: 'EC2A 4QX', city: 'London')
