5bc117ab64b91fa89af901df492bc534342f5467
Michael Dusong <michael@regiomag.net>
2016-08-26 14:41:37 +0100

    Seo implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  # Seo implementation
+  def yield_with_default(holder, default)
+    if content_for?(holder)
+      content_for(holder).squish
+    else
+      default
+    end
+  end
 end
