c2c926555fd9a2def8b03a74eb202e017f2f9cf0
Alex Benoit <alexander.benoit@outlook.com>
2016-08-26 10:26:41 +0100

    no auhenicate on index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :contact]
+  skip_before_action :authenticate_user!, only: [:home, :contact, :index]
   def index
     @events = Event.all
   end
